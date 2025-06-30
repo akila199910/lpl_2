046d765b2b1ea557e9bf7745e83acc3bc7f80b6d
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-06-30 19:41:25 +0530

    login user authemtication and  team routes create

--- client/src/router/teamRoutes.js
+++ client/src/router/teamRoutes.js
@@ -1,13 +1,21 @@
-import TeamCreate from "../pages/admin/team/TeamCreate.vue";
-import TeamList from "../pages/admin/team/TeamList.vue";
+import { requireAuth, requireRole } from './guards.js';
 
 export default [
-    {
-        path: "/teams",
-        component: TeamList,
-    },
-    {
-        path: "/teams/create",
-        component: TeamCreate,
-    }
+  {
+    path: '/teams',
+    component: () => import('../pages/admin/team/TeamList.vue'),
+    beforeEnter: requireAuth,
+  },
+  {
+    path: '/teams/create',
+    component: () => import('../pages/admin/team/TeamCreate.vue'),
+    beforeEnter: requireAuth,
+  },
+  {
+  path: '/teams/update/:id',
+  name: 'TeamEdit',
+  component: () => import('../pages/admin/team/TeamUpdate.vue'),
+  beforeEnter: requireAuth,
+}
+
 ];
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -12,4 +12,6 @@ const api = axios.create({
 export default {
   login: (data) => api.post('/auth/login', data),
   register: (data) => api.post('/auth/register', data),
+  getCurrentUser: () => api.get('/auth/me'),
+  logout: () => api.post('/auth/logout'),
 };
