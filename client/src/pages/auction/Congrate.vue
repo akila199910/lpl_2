e9a881769b5b856d3d5c07fbdc65da9bd99f4ecc
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-07-29 17:05:08 +0530

    Update Congrate.vue

--- client/src/pages/auction/Congrate.vue
+++ client/src/pages/auction/Congrate.vue
@@ -10,14 +10,14 @@
     <!-- Overlay content -->
     <div class="z-10 text-gray-800 px-4 max-w-xl">
       <img
-        src='../../assets/defaultUser.jpeg'
+        :src='displayData.player_id?.user_id?.profile.profileImageUrl == "user.png" ? defualtUser : displayData.player_id?.user_id?.profile.profileImageUrl'
         alt="Player"
         class="w-48 h-48 rounded-full mx-auto mb-6 border-4 border-black shadow-lg"
       />
-      <h1 class="text-2xl sm:text-5xl font-extrabold mb-3 mt-3 uppercase">Congratulations {{ displayData.player_id.user_id.name }} !</h1>
+      <h1 class="text-2xl sm:text-5xl font-extrabold mb-3 mt-3 uppercase">Congratulations {{ displayData.player_id?.user_id?.name }} !</h1>
       <p class="text-lg sm:text-3xl font-bold mb-2">You has been sold!</p>
       <p class="text-lg sm:text-3xl font-bold mb-2">Rs : {{ displayData.bid_value }}</p>
-      <p class="text-2xl sm:text-3xl font-bold mb-2 uppercase">{{ displayData.team_id.name }}</p>
+      <p class="text-2xl sm:text-3xl font-bold mb-2 uppercase">{{ displayData.team_id?.name }}</p>
     </div>
   </div>
 </template>
@@ -25,7 +25,8 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
-import { getPlayerBidDtails } from '../../services/autionService';
+import { getPlayerBidDetails } from '../../services/autionService';
+import defualtUser from '../../assets/defualtUser.jpeg';
 
 const route = useRoute()
 const displayData = ref({});
@@ -33,7 +34,7 @@ const displayData = ref({});
 onMounted(async () => {
   const playerId = route.params.id
   try {
-    const res = await getPlayerBidDtails(playerId)
+    const res = await getPlayerBidDetails(playerId)
     displayData.value = res.data.data;
 
     console.log(displayData.value)
